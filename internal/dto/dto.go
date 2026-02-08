2498a356c2571ce21a8794ed85b512672837a3be
Мавлоназаров Хушнуд <h.mavlonazarov@automacon.ru>
2026-02-08 22:36:42 +0300

    add prices

--- internal/dto/dto.go
+++ internal/dto/dto.go
@@ -1,10 +1,12 @@
 package dto
 
 type ProductInfo struct {
-	Id     string
-	Name   string
-	Weight string
-	Image  string
+	Id           string
+	Name         string
+	Weight       string
+	Image        string
+	OfferedPrice string
+	ListPrice    string
 }
 
 func NewProductInfo(id, name, weight, image string) ProductInfo {
@@ -14,4 +16,18 @@ func NewProductInfo(id, name, weight, image string) ProductInfo {
 		Weight: weight,
 		Image:  image,
 	}
+}
+
+func (i *ProductInfo) AddPriceInfo(prices PriceInfo) {
+	i.OfferedPrice = prices.OfferPrice
+	i.ListPrice = prices.ListPrice
+}
+
+type PricesDto struct {
+	Prices map[string]PriceInfo `json: "prices"`
+}
+
+type PriceInfo struct {
+	OfferPrice string `json: "offerPrice"`
+	ListPrice  string `json: "listPrice"`
 }
