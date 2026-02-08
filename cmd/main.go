2cff1fc731a05d1e785509b15ff8cbe55162e62a
Мавлоназаров Хушнуд <h.mavlonazarov@automacon.ru>
2026-02-09 00:45:51 +0300

    upd main and crawler interface

--- cmd/main.go
+++ cmd/main.go
@@ -1,104 +1,37 @@
 package main
 
 import (
-	"fmt"
-	"log"
-	"regexp"
-	"strconv"
-	"strings"
-	"time"
+	"context"
 
-	cu "github.com/Davincible/chromedp-undetected"
-	"github.com/PuerkitoBio/goquery"
+	"github.com/Raime-34/crawler.git/internal/dto"
 	"github.com/Raime-34/crawler.git/internal/okeycrawler"
-	"github.com/chromedp/cdproto/network"
-	"github.com/chromedp/chromedp"
 )
 
 func main() {
-	crawler := okeycrawler.NewOkeyCrawler()
-	err := crawler.LoadPages()
-	if err != nil {
-		fmt.Println(err)
-	}
-}
+	var crawler Crawler = okeycrawler.NewOkeyCrawler()
+	crawler.LoadMajorCategory()
+	// goods, err := crawler.LoadPages()
+	// if err != nil {
+	// 	fmt.Println(err)
+	// }
 
-func undetectedChromedp() {
-	ctx, cancel, err := cu.New(cu.NewConfig(
-		// cu.WithHeadless(),
-		cu.WithTimeout(10 * time.Second),
-	))
-	if err != nil {
-		panic(err)
-	}
-	defer cancel()
+	// fmt.Println("Сохраняем в файл...")
+	// data, _ := json.Marshal(goods)
 
-	chromedp.ListenTarget(ctx, func(ev any) {
-		if ev, ok := ev.(*network.EventResponseReceived); ok {
-			// fmt.Println(ev.Type)
-			// fmt.Println(ev.Response.URL)
+	// fmt.Println(len(goods))
 
-			if ev.Type != "Document" {
-				return
-			}
-
-			// go func() {
-			// 	c := chromedp.FromContext(ctx)
-			// 	rbp := network.GetResponseBody(ev.RequestID)
-			// 	body, err := rbp.Do(cdp.WithExecutor(ctx, c.Target))
-			// 	if err != nil {
-			// 		fmt.Println(ev.Type)
-			// 		fmt.Println("Ошибка: " + err.Error())
-			// 	}
-			// 	if err == nil {
-			// 		if bytes.Contains(body, []byte("FacetCount_updated")) {
-			// 			re := regexp.MustCompile(`totalCount\s*:\s*(\d+)`)
-			// 			m := re.FindSubmatch(body)
-			// 			if len(m) == 2 {
-			// 				fmt.Println(m)
-			// 				total, _ := strconv.Atoi(string(m[1]))
-			// 				log.Println("totalCount =", total)
-			// 			}
-			// 		}
-			// 	}
-			// }()
-		}
-	})
-
-	var htmlContent string
-	if err := chromedp.Run(
-		ctx,
-
-		network.Enable(),
-
-		chromedp.Navigate("https://www.okeydostavka.ru/msk/molochnye-produkty-iaitso/molochnye-produkty/moloko-i-slivki"),
-		chromedp.Sleep(2*time.Second),
-		chromedp.OuterHTML("html", &htmlContent, chromedp.ByQuery),
-	); err != nil {
-		panic(err)
-	}
-
-	log.Println(strings.Contains(htmlContent, "totalPages"))
-
-	reader := strings.NewReader(htmlContent)
-
-	re := regexp.MustCompile(`WCParamJS\.totalPages\s*=\s*'([\d.]+)'`)
-	m := re.FindStringSubmatch(htmlContent)
-	if len(m) > 1 {
-		fmt.Println("totalPages:", m[1])
-		f, _ := strconv.ParseFloat(m[1], 64)
-		fmt.Println(int(f))
-	}
-
-	doc, _ := goquery.NewDocumentFromReader(reader)
-
-	sel := doc.Find(".product_listing_container").First()
-
-	grid := sel.Children().First()
-
-	fmt.Println(grid.Children().Length())
+	// category := cfg.GetConfig().Category
+	// ind := strings.LastIndex(category, `/`)
+	// if ind > 0 {
+	// 	category = category[ind:]
+	// 	err = os.WriteFile(fmt.Sprintf("reports%v.json", category), data, 0644)
+	// 	if err != nil {
+	// 		fmt.Println(err)
+	// 	}
+	// }
 }
 
 type Crawler interface {
-	LoadPages() error
+	LoadMajorCategory() (map[string][]*dto.ProductInfo, error)
+	LoadPages(context.Context, string) ([]*dto.ProductInfo, error)
 }
