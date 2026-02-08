32be1853cf828ba886d2a63d21756ab08f648fd0
Мавлоназаров Хушнуд <h.mavlonazarov@automacon.ru>
2026-02-08 23:55:38 +0300

    fxd grid class for category with one page

--- internal/okeycrawler/consts.go
+++ internal/okeycrawler/consts.go
@@ -4,7 +4,7 @@ const (
 	okeyBaseUrl           = "https://www.okeydostavka.ru/msk/%v"
 	imageBaseUrl          = "https://www.okeydostavka.ru%v"
 	totalPagesRegexp      = `WCParamJS\.totalPages\s*=\s*'([\d.]+)'`
-	productContainerClass = ".grid_mode.grid.rows"
+	productContainerClass = ".grid_mode.grid"
 	okeyAmountOfGoods     = 72
 	okeyBaseFilter        = "#facet:&productBeginIndex:%v&orderBy:2&pageView:grid&pageSize:%v&"
 )
