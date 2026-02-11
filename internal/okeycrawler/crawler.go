8e92f52cbf9efd4cc7d400e7415760ea868351a7
Мавлоназаров Хушнуд <h.mavlonazarov@automacon.ru>
2026-02-11 10:51:22 +0300

    add store id and upd multipage catalog handling

--- internal/okeycrawler/crawler.go
+++ internal/okeycrawler/crawler.go
@@ -235,7 +235,7 @@ func (c *okeyCrawler) LoadMajorCategory() ([]dto.ProductInfo2, error) {
 	go c.emulateMouse(ctx)
 
 	// Загружаем первую страницу категории
-	initialPageUrl := fmt.Sprintf(okeyBaseUrl, config.Category)
+	initialPageUrl := fmt.Sprintf(okeyBaseUrl, config.Category, config.StoreId)
 	fmt.Printf("Загружаеим начальную страницу: %s\n", initialPageUrl)
 
 	html, err := c.loadPage(ctx, initialPageUrl)
@@ -342,41 +342,145 @@ func (c *okeyCrawler) handleMinorCategoryPage(ctx context.Context) {
 
 	// nameXPath := itemLinkXPath + `//div[contains(@class,'product-name')]//a[@href and @title]`
 
-	var n int
-	_ = chromedp.Run(ctx,
-		chromedp.Evaluate(`document.querySelectorAll("`+listCSS+` li .product-name a[title][href]").length`, &n),
-	)
+	currentPage := 1
+	isOnePageCatalog := false
+	for {
+		pagingRoot := `(//div[contains(@class,'paging_controls')])[1]`
+		activeXPath := pagingRoot + `//a[contains(@class,'active') and contains(@class,'selected')]`
 
-	for i := 1; i <= n; i++ {
-		sel := fmt.Sprintf(itemLinkXPath, i)
+		fmt.Println("Ожидаем контроллера страниц")
+		dCtx, cancel := context.WithDeadline(ctx, time.Now().Add(10*time.Second))
+		chromedp.Run(
+			dCtx,
+			chromedp.WaitVisible(".paging_controls a.active.selected", chromedp.ByQuery),
+		)
+		cancel()
+		fmt.Println("Контроллер найден")
 
-		var name string
-		var html string
-		err := chromedp.Run(ctx,
-			chromedp.WaitVisible(listCSS, chromedp.ByQuery),
-			chromedp.ScrollIntoView(sel, chromedp.BySearch),
-			chromedp.Sleep(time.Duration(1200+rand.Intn(2600))*time.Millisecond),
-
+		var roots []*cdp.Node
+		_ = chromedp.Run(
+			ctx,
 			chromedp.WaitReady("body"),
-			chromedp.AttributeValue(sel, "title", &name, nil, chromedp.BySearch),
-			chromedp.ActionFunc(func(ctx context.Context) error {
-				fmt.Printf("	∟ переход на страницу товара %v\n", name)
-				return nil
-			}),
-			chromedp.Click(sel, chromedp.BySearch),
-			chromedp.WaitVisible("div.product-name a[title]", chromedp.ByQuery),
-			chromedp.OuterHTML("html", &html, chromedp.ByQuery),
-			chromedp.ActionFunc(func(ctx context.Context) error {
-				c.handlePage(ctx, html, minorPageType)
-				return nil
-			}),
+			chromedp.Nodes(pagingRoot, &roots, chromedp.BySearch, chromedp.AtLeast(0)),
+		)
+		if len(roots) == 0 {
+			isOnePageCatalog = true
+		}
+		if isOnePageCatalog {
+			fmt.Println("Страница классифицирона как одностраничная")
+		} else {
+			fmt.Println("Обнаружено множество страниц")
+		}
 
-			chromedp.NavigateBack(),
+		if !isOnePageCatalog {
+			activeXPath2 := `(//div[contains(@class,'paging_controls')])[1]//a[contains(@class,'active') and contains(@class,'selected')]`
+			var pageStr string
+			_ = chromedp.Run(ctx,
+				chromedp.Text(activeXPath2, &pageStr, chromedp.BySearch),
+			)
+
+			page, _ := strconv.Atoi(strings.TrimSpace(pageStr))
+
+			if page != currentPage {
+				continue
+			}
+		}
+
+		fmt.Printf("		текущая страница: %v\n", currentPage)
+		var n int
+		_ = chromedp.Run(ctx,
+			chromedp.WaitVisible(".productListingWidget", chromedp.ByQuery),
+			chromedp.Evaluate(`document.querySelectorAll("`+listCSS+` li .product-name a[title][href]").length`, &n),
+		)
+
+		fmt.Println("разбираем товары")
+		// Проходимся по карточкам товаров
+		for i := 1; i <= n; i++ {
+			sel := fmt.Sprintf(itemLinkXPath, i)
+
+			var name string
+			var html string
+			err := chromedp.Run(ctx,
+				chromedp.WaitVisible(listCSS, chromedp.ByQuery),
+				chromedp.ScrollIntoView(sel, chromedp.BySearch),
+				chromedp.Sleep(time.Duration(1200+rand.Intn(2600))*time.Millisecond),
+
+				chromedp.WaitReady("body"),
+				chromedp.AttributeValue(sel, "title", &name, nil, chromedp.BySearch),
+				chromedp.ActionFunc(func(ctx context.Context) error {
+					fmt.Printf("	∟ переход на страницу товара %v\n", name)
+					return nil
+				}),
+				chromedp.Click(sel, chromedp.BySearch),
+				chromedp.WaitVisible("div.product-name a[title]", chromedp.ByQuery),
+				chromedp.OuterHTML("html", &html, chromedp.ByQuery),
+				chromedp.ActionFunc(func(ctx context.Context) error {
+					c.handlePage(ctx, html, minorPageType)
+					return nil
+				}),
+
+				chromedp.NavigateBack(),
+				chromedp.WaitVisible(listCSS, chromedp.ByQuery),
+				chromedp.Sleep(1*time.Second),
+			)
+			if err != nil {
+				continue
+			}
+		}
+
+		if isOnePageCatalog {
+			break
+		}
+
+		nextXPath := activeXPath + `/following-sibling::a[contains(@class,'hoverover')][1]`
+		var hasNext bool
+		js := fmt.Sprintf(`
+		(function () {
+			return document
+			.evaluate(
+				%q,
+				document,
+				null,
+				XPathResult.FIRST_ORDERED_NODE_TYPE,
+				null
+			)
+			.singleNodeValue !== null;
+		})()
+		`, nextXPath)
+		_ = chromedp.Run(ctx,
+			chromedp.EvaluateAsDevTools(js, &hasNext),
+		)
+
+		if !hasNext {
+			break
+		}
+
+		var before string
+		_ = chromedp.Run(ctx,
+			chromedp.Text(activeXPath, &before, chromedp.BySearch),
+		)
+
+		currentPage++
+		err := chromedp.Run(ctx,
+			chromedp.ScrollIntoView(nextXPath, chromedp.BySearch),
+			chromedp.Sleep(time.Duration(1200+rand.Intn(2600))*time.Millisecond),
+			chromedp.Click(nextXPath, chromedp.BySearch),
 			chromedp.WaitVisible(listCSS, chromedp.ByQuery),
 		)
 		if err != nil {
-			continue
+			break
 		}
+	}
+
+	fmt.Println("Minor page processed")
+	for i := 0; i < currentPage-1; i++ {
+		fmt.Println("NavigateBack()")
+		chromedp.Run(
+			ctx,
+			chromedp.NavigateBack(),
+			chromedp.Sleep(1*time.Second),
+			chromedp.WaitReady("body"),
+		)
 	}
 }
 
@@ -385,6 +489,8 @@ func txt(s string) string { return strings.TrimSpace(strings.ReplaceAll(s, "\u00
 func (c *okeyCrawler) handleMainProduectPage(ctx context.Context) dto.ProductInfo2 {
 	var html string
 	_ = chromedp.Run(ctx,
+		chromedp.Sleep(2*time.Second),
+		chromedp.WaitVisible(".product_main_info", chromedp.ByQuery),
 		chromedp.OuterHTML("html", &html, chromedp.ByQuery),
 	)
 
@@ -442,7 +548,7 @@ func (c *okeyCrawler) handleMainProduectPage(ctx context.Context) dto.ProductInf
 		}
 	})
 
-	fmt.Printf("Извлеченные данные из страницы %v\n", p)
+	// fmt.Printf("Извлеченные данные из страницы %v\n", p)
 	return p
 }
 
