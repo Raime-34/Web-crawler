1fc35029752871aad726bfc5f43aa36f33d55154
Мавлоназаров Хушнуд <h.mavlonazarov@automacon.ru>
2026-02-11 10:49:41 +0300

    add store id

--- internal/cfg/congif.go
+++ internal/cfg/congif.go
@@ -13,6 +13,7 @@ var (
 type Cfg struct {
 	Headless bool
 	Category string
+	StoreId  string
 }
 
 func GetConfig() *Cfg {
@@ -21,6 +22,7 @@ func GetConfig() *Cfg {
 
 		flag.BoolVar(&newCfg.Headless, "h", false, "Флаг для открытия браузера во время сбора данных. На Windows этот флаг лучше оставить дефолтным")
 		flag.StringVar(&newCfg.Category, "c", "molochnye-produkty-iaitso/molochnye-produkty/moloko-i-slivki", "Название категории, к примеру для https://www.okeydostavka.ru/msk/molochnye-produkty-iaitso/molochnye-produkty/moloko-i-slivki это molochnye-produkty-iaitso/molochnye-produkty/moloko-i-slivki")
+		flag.StringVar(&newCfg.StoreId, "s", "10151", "ID магазина для которого нужно собрать данные. По умолчанию это ГМ 'Альтуфьево'")
 		flag.Parse()
 
 		cfg = &newCfg
